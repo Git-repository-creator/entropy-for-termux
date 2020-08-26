2b6b4f164bf86d175bd3cc3d085ca1136cb948c4
Git <59158864+Git-repository-creator@users.noreply.github.com>
2020-08-26 21:29:00 +0300

    Updated entropy.sh
    Updated to actual API key

--- entropy.sh
+++ entropy.sh
@@ -1,3 +1,3 @@
 cd ~/entropy-for-termux
 clear
-python3 entropy -b 2 -v --shodan "RHNWdCNhYu4Fpb2DV7lM43AqQXTNBnM7" | grep [+]
+python3 entropy -b 2 -v --shodan "PSKINdQe1GyxGgecYz2191H2JoS9qvgD" | grep [+]
